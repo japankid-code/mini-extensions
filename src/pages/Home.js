3ab9fa2d5585bc2502d66339473be6b0240af905
japankid-code <japankid.jake@gmail.com>
2021-08-15 11:26:25 -0500

    :package: add react-dotenv

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
+import env from "react-dotenv";
+
+const API_KEY = env.API_KEY;
+
 export default function Home() {
   async function data() {
     return await fetch(
       "https://api.airtable.com/v0/meta/bases/app8ZbcPx7dkpOnP0/tables"
     ).then((response) => response.json());
   }
+  console.log(API_KEY);
   console.log(data());
   return <div className="home">My Home Page</div>;
 }
